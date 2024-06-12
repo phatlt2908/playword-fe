bb93d25da704dcf301ebf425ca2ffd63f88acf9b
phatlt <phatlt@jienielab.asia>
2024-06-12 15:13:04 +0700

    update WordLink logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,100 +1,35 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./page.module.scss";
+
+const wordLinkLogo = "/word-link-logo.jpg";
 
 export default function Home() {
   return (
     <>
       <div className="columns is-multiline is-centered is-mobile w-100">
-        <div
-          id="splatoon"
-          className="column is-one-fifth-widescreen is-one-quarter-desktop is-one-third-tablet is-half-mobile"
+        <Link
+          href="/noi-tu"
+          id="word-link"
+          className="cursor-pointer column is-one-quarter-widescreen is-one-third-desktop is-one-third-tablet is-half-mobile"
         >
           <div className="card">
             <div className="card-image">
               <figure className="image is-1by1">
                 <Image
-                  src="https://upload.wikimedia.org/wikipedia/en/4/49/Splatoon_2.jpg"
-                  alt="Splatoon 2"
+                  src={wordLinkLogo}
+                  alt="Noi tu"
                   width={500}
                   height={500}
                   priority
                 />
               </figure>
             </div>
-          </div>
-        </div>
-        <div
-          id="super-smash-bros"
-          className="column is-one-fifth-widescreen is-one-quarter-desktop is-one-third-tablet is-half-mobile"
-        >
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-1by1">
-                <Image
-                  src="https://img-eshop.cdn.nintendo.net/i/08af58551a19df2a73ccb36f720388434a1965776b34675c6f69af3f93280330.jpg"
-                  alt="Super Smash Bros Ultimate"
-                  width={500}
-                  height={500}
-                  priority
-                />
-              </figure>
+            <div className="card-content has-text-centered title is-size-6 p-3">
+              Nối từ
             </div>
           </div>
-        </div>
-        <div
-          id="breath-of-the-wild"
-          className="column is-one-fifth-widescreen is-one-quarter-desktop is-one-third-tablet is-half-mobile"
-        >
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-1by1">
-                <Image
-                  src="https://i2.wp.com/fantasy-hive.co.uk/wp-content/uploads/2017/09/Zelda-Breath-of-the-Wild-Thumbnail.jpg?resize=300%2C300&ssl=1"
-                  alt="Breath of the Wild"
-                  width={500}
-                  height={500}
-                  priority
-                />
-              </figure>
-            </div>
-          </div>
-        </div>
-        <div
-          id="celeste"
-          className="column is-one-fifth-widescreen is-one-quarter-desktop is-one-third-tablet is-half-mobile"
-        >
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-1by1">
-                <Image
-                  src="https://publish.one37pm.net/wp-content/uploads/2018/12/celeste-mobile.jpg"
-                  alt="Celeste"
-                  width={500}
-                  height={500}
-                  priority
-                />
-              </figure>
-            </div>
-          </div>
-        </div>
-        <div
-          id="animal-crossing"
-          className="column is-one-fifth-widescreen is-one-quarter-desktop is-one-third-tablet is-half-mobile"
-        >
-          <div className="card">
-            <div className="card-image">
-              <figure className="image is-1by1">
-                <Image
-                  src="https://upload.wikimedia.org/wikipedia/en/1/1f/Animal_Crossing_New_Horizons.jpg"
-                  alt="Animal Crossing New Horizons"
-                  width={500}
-                  height={500}
-                  priority
-                />
-              </figure>
-            </div>
-          </div>
-        </div>
+        </Link>
       </div>
     </>
   );
