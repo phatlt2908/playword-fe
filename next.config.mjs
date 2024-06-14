d372cd12d700e491ecaae673a40b39dbd4880d6f
phatlt <phatlt@jienielab.asia>
2024-06-14 14:19:47 +0700

    chore: Remove unused CSS class and update npm dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { hostname: "upload.wikimedia.org" },
-      { hostname: "img-eshop.cdn.nintendo.net" },
-      { hostname: "i2.wp.com" },
-      { hostname: "publish.one37pm.net" },
     ],
   },
-
-  reactStrictMode: false,
+  reactStrictMode: false
 };
 
 export default nextConfig;
---
+++ src/app/chat-demo/page.js
@@ -0,0 +1,84 @@
+"use client";
+
+const { useState, useEffect } = require("react");
+import { Stomp } from "@stomp/stompjs";
+import * as SockJS from "sockjs-client";
+
+const ChatDemo = () => {
+  const [username, setUsername] = useState("Phat");
+  const [messageList, setMessageList] = useState([]);
+  const [stompClient, setStompClient] = useState();
+
+  useEffect(() => {
+    if (stompClient) {
+      stompClient.connect({}, onConnected, onError);
+    }
+  }, [stompClient]);
+
+  const connect = () => {
+    console.log("Connecting websocket...");
+    var socket = new SockJS("http://localhost:8081/ws");
+    setStompClient(Stomp.over(socket));
+  };
+
+  const onConnected = () => {
+    console.log("stompClient: ", stompClient);
+    stompClient.subscribe("/room/1234", onMessageReceived);
+
+    stompClient.send(
+      "/app/chat.addUser/1234",
+      {},
+      JSON.stringify({ sender: username, type: "JOIN" })
+    );
+  };
+
+  const onError = (error) => {
+    console.error("Error: ", error);
+  };
+
+  const onMessageReceived = (payload) => {
+    var res = JSON.parse(payload.body);
+    console.log("Received message >>> : ", res);
+    let message = res.sender + ": " + res.message;
+    setMessageList((prev) => [...prev, message]);
+  };
+
+  const sendMessage = (message) => {
+    console.log("Sending message...", stompClient);
+    if (message && stompClient) {
+      var chatMessage = {
+        sender: username,
+        message: message,
+        type: "CHAT",
+      };
+      stompClient.send(
+        "/app/chat.sendMessage/1234",
+        {},
+        JSON.stringify(chatMessage)
+      );
+    }
+  };
+
+  useEffect(() => {
+    connect();
+  }, []);
+
+  return (
+    <div>
+      <button
+        className="button is-primary"
+        onClick={() => sendMessage("Test message ")}
+      >
+        Add message
+      </button>
+      <div>Message:</div>
+      <div>
+        {messageList.map((message, index) => (
+          <div key={index}>{message + index}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ChatDemo;
