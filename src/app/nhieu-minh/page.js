7edcf184cb74a8425d202cb6aef7a23b817eb3ab
phatlt <phatlt@jienielab.asia>
2024-07-15 14:28:43 +0700

    chore: Update page.js export function name for consistency

--- src/app/nhieu-minh/page.js
+++ src/app/nhieu-minh/page.js
@@ -3,7 +3,7 @@ import WordLinkMultiLobby from "@/components/contents/multiple-mode-lobby";
 export const metadata = {
   title: "Nối từ nhóm | Nối từ vui",
   description:
-    "Chơi nối từ nhóm online cùng nhau, theo cặp hoặc theo nhóm nhiều người. Đánh bại các đối thủ và trở thành người chơi còn lại cuối cùng để giành chiến thắng",
+    "Chơi nối từ nhóm online cùng nhau, solo theo cặp hoặc theo nhóm nhiều người. Đánh bại các đối thủ và trở thành người chơi còn lại cuối cùng để giành chiến thắng",
 };
 
 export default function MultipleModeLobbyPage() {
